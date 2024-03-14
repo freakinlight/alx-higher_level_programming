9c9a02cfbcce35138c7201d48eee9a7e621e7634
freakinlight <mazouznour0@gmail.com>
2024-03-14 13:49:12 -0700

    5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-import importlib.util
-
-def main():
-    spec = importlib.util.spec_from_file_location("variable_load_5", "./variable_load_5.py")
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    print(module.a)
 
 if __name__ == "__main__":
-    main()
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
