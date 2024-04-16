5c5def8f2c574f4877a0daedef0a40af823c6f00
freakinlight <mazouznour0@gmail.com>
2024-04-16 16:41:58 -0700

    11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Defines a class S."""
+
+
+class Student:
+    """Represent a S."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new S.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Dictionary representation of the S.
+
+        If attrs is a list of strings, represents only attributes
+        included in da list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace attributes of the S.
+
+        Args:
+            json (dict): The key/value pairs to replace attributes with.
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
