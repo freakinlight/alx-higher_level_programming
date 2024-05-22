d10d28f14f9d6282e05aeffe56a0f92b982d7e9a
freakinlight <mazouznour0@gmail.com>
2024-05-22 14:27:21 -0700

    102

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
--- A script
-SELECT tv_shows.title, IFNULL(SUM(tv_show_ratings.rating), 0) AS rating
-FROM tv_shows
-LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_shows.id
-ORDER BY rating DESC, tv_shows.title ASC;
+-- A script 
+SELECT t.title, IFNULL(SUM(r.rate), 0) AS rating
+FROM tv_shows AS t
+LEFT JOIN tv_show_ratings AS r ON t.id = r.show_id
+GROUP BY t.id
+ORDER BY rating DESC;
