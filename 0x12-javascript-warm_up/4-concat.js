9f2e9184c4868907bc324a05a104dce692bdb42e
freakinlight <mazouznour0@gmail.com>
2024-06-18 14:38:33 -0700

    bhkj

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,11 +1,6 @@
 #!/usr/bin/env node
 
-// Retrieve the arguments passed to the script
 const args = process.argv.slice(2);
-
-// Access the first and second arguments
 const firstArg = args[0];
 const secondArg = args[1];
-
-// Print the arguments in the specified format
 console.log(`${firstArg} is ${secondArg}`);
